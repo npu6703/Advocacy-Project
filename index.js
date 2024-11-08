233d5d45502239584eb6b378f9180d32b38c4493
npu6703 <phuonguyen672003@gmail.com>
2024-11-08 13:09:40 -0600

    Added image folder

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ themeButton.addEventListener("click", toggleDarkMode);
 
 let signNowButton = document.querySelector("#sign-now-button")
 const addSignature = (event) => {
-    event.preventDefault; 
+    // event.preventDefault; 
     const name = document.getElementById("name").value;
     const email = document.getElementById("email").value;
     const craft = document.getElementById("craft").value;
@@ -23,4 +23,50 @@ signaturesSection.appendChild(newSignature);
 document.getElementById("sign-petition").reset();
 }
 // Add event listener for the Sign Now button to trigger the addSignature function
-signNowButton.addEventListener("click", addSignature);
+// signNowButton.addEventListener("click", addSignature);
+
+
+
+
+const validateForm = () => {
+
+    let containsErrors = false;
+  
+    var petitionInputs = document.getElementById("sign-petition").elements;
+    // TODO: Loop through all inputs
+    for(let i = 0; i < petitionInputs.length; i++){
+        if (petitionInputs[i].value.length < 2) {
+            containsErrors = true; 
+            petitionInputs[i].classList.add('error');
+            
+        }
+        else {
+            petitionInputs[i].classList.remove('error');
+          }
+    }
+
+    if(containsErrors == false){
+        addSignature(); 
+        for(let i = 0; i < petitionInputs.length; i++){
+            petitionInputs[i].value = "";
+            containsErrors = false;
+        }
+    }
+    const email = document.getElementById('email');
+    if (!email.value.includes('.com')) {
+        containsErrors - true; 
+        email.classList.add('error');
+    }
+    else {
+        email.classList.remove('error');
+    }
+    
+    // TODO: Validate the value of each input
+  
+  
+  
+    // TODO: Call addSignature() and clear fields if no errors
+  
+  }
+  
+  signNowButton.addEventListener('click', validateForm);
