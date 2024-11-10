735e473fe1256b519aeb007751da95a5f7a74d4f
npu6703 <phuonguyen672003@gmail.com>
2024-11-09 23:38:15 -0600

    Updated 3 files

--- index.js
+++ index.js
@@ -70,3 +70,29 @@ const validateForm = () => {
   }
   
   signNowButton.addEventListener('click', validateForm);
+
+
+  let animation ={
+    revealDistance: 150,
+    initualOpacity: 0,
+    transitionDelay: 0,
+    transitionDuration: '2s',
+    transitionProperty: 'all',
+    transitionTimingFunction: 'ease',
+  }
+
+  
+  let revealableContainers = document.querySelectorAll(".revealable"); 
+  const reveal = () => {
+    for(i =0; i < revealableContainers.length; i++){
+        let windowHeight = window.innerHeight;
+        let topOfRevealableContainer = revealableContainers[i].getBoundingClientRect().top;
+        if (topOfRevealableContainer < windowHeight - animation.revealDistance) {
+            revealableContainers[i].classList.add("active"); 
+          } else {
+            revealableContainers[i].classList.remove("active"); 
+          }
+    }
+  };
+
+  window.addEventListener("scroll", reveal);
